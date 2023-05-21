60d12003331844d390b6e531f4cbc360a0a5aa0e
Yuejia Teng <12127573+tengyuejia@users.noreply.github.com>
2023-05-21 14:33:16 -0500

    Update Create_Stacked_Barplots.R

--- Create_Stacked_Barplots.R
+++ Create_Stacked_Barplots.R
@@ -64,3 +64,17 @@ combined_graph <- ggplot(data_percent, aes(x = respondent_type, y = percentage, 
   facet_wrap(~ item)
 
 print(combined_graph)
+
+# Copyright 2023 Yuejia Teng, Ph.D.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
